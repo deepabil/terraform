2cf087bcd41bb9d434f908d9a422e563c618ea42
deepachigale <deepa.chigale@bilvantis.io>
2024-08-30 16:27:47 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,18 +1,20 @@
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
-}
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   default     = "data.aws_ssm_parameter.latest_ami.value" # Default Amazon Linux 2 AMI
 }
 
-variable "subnet_id" {
-  description = "Subnet ID for the instance"
+variable "instance_type" {
+  description = "Type of instance to create"
+  type        = string
 }
 
+variable "subnet_id" {
+  description = "The ID of the subnet in which to launch the instance"
+  type        = string
+}
 
 variable "security_group_id" {
-  description = "Security group ID"
+  description = "The ID of the security group to associate with the instance"
+  type        = string
 }
