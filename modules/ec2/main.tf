699e8123acc25e85fbe0f24bc08a02a862c7ce5a
deepachigale <deepa.chigale@bilvantis.io>
2024-08-30 16:28:30 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "test_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_id
+  security_groups = [var.security_group_id]
 
 
   tags = {
