ce7dcafc6c369e41e3f75d40e27f329c3367cd9f
deepachigale <deepa.chigale@bilvantis.io>
2024-08-30 16:48:53 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
-  default     = "my-test-bucket"  # Must be unique globally
+  type        = string
+  default = "terrafor_test_bucket"
 }
-
