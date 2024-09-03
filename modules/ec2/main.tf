7050e4adfe86f8ae57b226dd79a6e0ce86216334
deepachigale <deepa.chigale@bilvantis.io>
2024-09-03 15:29:00 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,14 +1,13 @@
-# Fetch the latest Amazon Linux 2 AMI ID using AWS Systems Manager Parameter Store
+
 data "aws_ssm_parameter" "latest_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 resource "aws_instance" "test_instance" {
-  ami                    = data.aws_ssm_parameter.latest_ami.value  # Use the fetched AMI ID here
+  ami                    = data.aws_ssm_parameter.latest_ami.value
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  security_groups = [var.security_group_id]
-
+  vpc_security_group_ids = [var.security_group_id]  # Use security group IDs here
 
   tags = {
     Name = "Terraform-EC2"
