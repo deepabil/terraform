0514057e425846878231742edf00fa6c41cf43f8
deepachigale <deepa.chigale@bilvantis.io>
2024-09-03 15:47:52 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,7 +7,8 @@ resource "aws_instance" "test_instance" {
   ami                    = data.aws_ssm_parameter.latest_ami.value
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]  # Use security group IDs here
+  # Conditionally include security group ID in the list
+  vpc_security_group_ids = var.security_group_id != "" ? [var.security_group_id] : []
 
   tags = {
     Name = "Terraform-EC2"
