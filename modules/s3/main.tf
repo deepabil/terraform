2594b744a386c89d1e1dc69aff2dc0839d35df37
deepachigale <deepa.chigale@bilvantis.io>
2024-09-03 15:03:36 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,11 @@
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 resource "aws_s3_bucket" "storage_bucket" {
-  bucket = var.bucket_name
+  # Combine base name with a unique suffix to ensure uniqueness
+  bucket = "${var.bucket_name}-${random_string.suffix.result}"
 
   tags = {
     Name = "Terraform-S3"
